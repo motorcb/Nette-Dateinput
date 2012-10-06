b0f4261e3b2baf7b75617e4e7ec14a3667250022
motorcb <motorcb@seznam.cz>
2012-10-06 10:01:47 +0200

    czech language

--- www/js/nette.js
+++ www/js/nette.js
@@ -12,7 +12,25 @@ $(function() {
         ampm: false
     };
     $.timepicker.setDefaults($.timepicker.regional['cs']);
-
+    
+    $.datepicker.regional['cs'] = {
+        closeText: 'Zavřít',
+        prevText: '&#x3c;Předchozí',
+        nextText: 'Následující&#x3e;',
+        currentText: 'Nyní',
+        monthNames: ['leden', 'únor', 'březen', 'duben', 'květen', 'červen', 'červenec', 'srpen','září', 'říjen', 'listopad', 'prosinec'],
+        monthNamesShort: ['led', 'úno', 'bře', 'dub', 'kvě', 'čer', 'čvc', 'srp', 'zář', 'říj', 'lis', 'pro'],
+        dayNames: ['neděle', 'pondělí', 'úterý', 'středa', 'čtvrtek', 'pátek', 'sobota'],
+        dayNamesShort: ['ne', 'po', 'út', 'st', 'čt', 'pá', 'so'],
+        dayNamesMin: ['ne', 'po', 'út', 'st', 'čt', 'pá', 'so'],
+        weekHeader: 'Týd',
+        dateFormat: 'dd.mm.yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+    $.datepicker.setDefaults($.datepicker.regional['cs']);
 
     $('input[data-dateinput-type]').dateinput({
         datetime: {
